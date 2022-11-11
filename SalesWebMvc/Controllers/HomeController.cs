cddbb93cc695e0b9343b1f78366b0b5c2c33b221
Marcelo <marcelogamedev99@gmail.com>
2022-11-11 17:36:49 +0000

    Department Model Class creation

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Web Sales Mvc App ";
+            ViewData["Author"] = "Marcelo Laranjeira";
 
             return View();
         }
